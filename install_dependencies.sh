b786b74bafb53dfbd04666669fd98bf4d0e45dd9
gkhanh <gkhanhle52@gmail.com>
2024-10-24 07:28:53 +0200

    add installing the sklearn library

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,5 +10,6 @@ apt-get install python3 python3-pip -y
 
 echo "Installing required Python libraries..."
 pip3 install opencv-python numpy
+pip install scikit-learn matplotlib opencv-python numpy
 
 echo "All dependencies installed successfully."
