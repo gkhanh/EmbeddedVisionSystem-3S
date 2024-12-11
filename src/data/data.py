6f57ae533acd8bfb7ac5e14c68be4c11991d6015
gkhanh <gkhanhle52@gmail.com>
2024-12-11 07:52:53 +0100

    add more sets of points for testing

--- src/data/data.py
+++ src/data/data.py
@@ -1,5 +1,6 @@
 # define points
 
+# Data set 1: Rotation by 45 degrees
 manipulation_points = [
     (0, 0),  # M0: Origin point of manipulation axis map
     (3, 9),  # M1
@@ -13,6 +14,7 @@ camera_points = [
     (4, 3),  # C3
 ]
 
+# Data set 2: Default - rotation by 0 degree
 # manipulation_points = [
 #     (0, 0),  # M0: Origin point of manipulation axis map
 #     (1, 0),  # M1
@@ -21,7 +23,37 @@ camera_points = [
 # ]
 #
 # camera_points = [
-#     (1, 1),  # C1
-#     (2, 1),  # C2
-#     (1, 2),  # C3
+#     (2, 1),  # C1
+#     (1, 2),  # C2
+#     (2, 2),  # C3
 # ]
+
+# Data set 3: Rotation by 30 degrees
+# manipulation_points = [
+#     (0, 0),  # M0: Origin
+#     (2, 5),  # M1
+#     (7, 3),  # M2
+#     (3, 2)  # M3
+# ]
+#
+# camera_points = [
+#     (8.464, 20.66),  # C1: Corresponds to M1
+#     (19.124, 22.196),  # C2: Corresponds to M2
+#     (13.196, 16.464)  # C3: Corresponds to M3
+# ]
+
+# Data set 4: Rotation by 90 degrees
+# manipulation_points = [
+#     (0, 0),  # M0: Origin
+#     (4, 0),  # M1: 4 units along X-axis
+#     (6, 3),  # M2: moves diagonally from M1
+#     (2, 3)   # M3: forms a trapezoid shape with the others
+# ]
+#
+# camera_points = [
+#     (10, 18),  # C1 corresponds to M1
+#     (4, 22),   # C2 corresponds to M2
+#     (4, 14)    # C3 corresponds to M3
+# ]
+
+# Data set 5: Rotation by 60 degrees
